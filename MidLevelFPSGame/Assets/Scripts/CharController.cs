c9b5f8850a4e69b5761d6ceb1891986dce7efc71
Fluke988 <jagtaphrushi@gmail.com>
2022-01-24 11:42:18 +0530

    Jump Raycasting Done

--- MidLevelFPSGame/Assets/Scripts/CharController.cs
+++ MidLevelFPSGame/Assets/Scripts/CharController.cs
@@ -7,10 +7,13 @@ public class CharController : MonoBehaviour
     public float playerSpeed;
     Rigidbody rb;
     public float playerJumpValue;
+    private bool isGrounded;
+    private CapsuleCollider capsuleCollider;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();   
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
     void Start()
@@ -28,17 +31,38 @@ public class CharController : MonoBehaviour
         PlayerJumpMovement();
     }
 
+    bool PlayerGrounded()
+    {
+        RaycastHit hitInfo;
+
+        if(Physics.SphereCast(transform.position, capsuleCollider.radius, Vector3.down, out hitInfo, (capsuleCollider.height/2.0f) - capsuleCollider.radius+0.1f ))
+        {
+            print("true statement");
+            return true;
+            
+        }
+        else
+        {
+            print("false statement");
+            return false;
+        }
+    }
+
     void PlayerMovement()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed /** Time.deltaTime*/;
-        float forwardMovement = Input.GetAxis("Vertical") * playerSpeed /** Time.deltaTime*/;
+        float horizontalMovement = Input.GetAxis("Horizontal") * playerSpeed;
+        float forwardMovement = Input.GetAxis("Vertical") * playerSpeed;
 
         transform.position += new Vector3(horizontalMovement, 0, forwardMovement);
     }
 
     void PlayerJumpMovement()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && PlayerGrounded())
+        {
+            print("jump pressed");
             rb.AddForce(0, playerJumpValue, 0);
+            //rb.velocity = new Vector3(0, playerJumpValue, 0);
+        }
     }
 }
