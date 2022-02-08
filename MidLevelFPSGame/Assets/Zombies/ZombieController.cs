60f8ec2c3d1dc34e711e9a0372feb342ea20dbd3
Fluke988 <jagtaphrushi@gmail.com>
2022-02-08 10:15:02 +0530

    Wander state to idle state and attack state done

--- MidLevelFPSGame/Assets/Zombies/ZombieController.cs
+++ MidLevelFPSGame/Assets/Zombies/ZombieController.cs
@@ -20,6 +20,7 @@ public class ZombieController : MonoBehaviour
     Animator anim;
     public GameObject targetPlayer;
     NavMeshAgent enemyAgent;
+    public float walkingSpeed, runningSpeed;
 
     enum STATE { IDLE, WANDER, CHASE, ATTACK, DEATH }
     STATE state = STATE.IDLE;
@@ -44,7 +45,7 @@ public class ZombieController : MonoBehaviour
     {
         //Logic for Zombie to see the player and chase
         //Need to calculate the distance between the zombie and the player
-        if (DistanceToPlayer()<10.0f)
+        if (DistanceToPlayer() > 10.0f)
         {
             return true;
         }
@@ -56,44 +57,73 @@ public class ZombieController : MonoBehaviour
     {
         return Vector3.Distance(targetPlayer.transform.position, this.transform.position);
     }
+    bool isVisible()
+    {
+        //logic for zombie to see the player and chase
+        if (DistanceToPlayer() < 10.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     void Update()
     {
         switch (state)
         {
             case STATE.IDLE:
-                if (ZombieCanSeePlayer())
+                if (isVisible())
                 {
                     state = STATE.CHASE;
                 }
-                else
+                else if(Random.Range(0,500) < 5)
                 {
                     state = STATE.WANDER;
                 }
                 break;
 
             case STATE.WANDER:
-                if(!enemyAgent.hasPath)
+                if (!enemyAgent.hasPath)
                 {
-                    float newRandPosX = this.transform.position.x + Random.Range(-10, 10);
-                    float newRandPosZ = this.transform.position.z + Random.Range(-10, 10);
-                    float newRandPosY = Terrain.activeTerrain.SampleHeight(new Vector3(newRandPosX, 0, newRandPosZ));
-                    Vector3 finalDestination = new Vector3(newRandPosX, newRandPosY, newRandPosZ);
+                    float newRandompositionX = this.transform.position.x + Random.Range(-10, 10);
+                    float newRandompositionZ = this.transform.position.z + Random.Range(-10, 10);
+                    float newRandompositionY = Terrain.activeTerrain.SampleHeight(new Vector3(newRandompositionX, 0, newRandompositionZ));
+                    Vector3 finalDestination = new Vector3(newRandompositionX, newRandompositionY, newRandompositionZ);
                     enemyAgent.SetDestination(finalDestination);
-                    enemyAgent.stoppingDistance = 3.5f;
+                    enemyAgent.stoppingDistance = 0f;
                     TurnOffAnimTriggers();
+                    enemyAgent.speed = walkingSpeed;
                     anim.SetBool("isWalking", true);
+                }
+                else if (isVisible())
+                {
+                    state = STATE.CHASE;
+                }
+                else if(Random.Range(0,100)<5)
+                {
+                    state=STATE.IDLE;
+                    TurnOffAnimTriggers();
+                    enemyAgent.ResetPath();
                 }
                 break;
 
             case STATE.CHASE:
                 enemyAgent.SetDestination(targetPlayer.transform.position);
-                enemyAgent.stoppingDistance = 3.5f;
+                enemyAgent.stoppingDistance = 2.0f;
+                enemyAgent.speed = runningSpeed;
                 TurnOffAnimTriggers();
                 anim.SetBool("isRunning", true);
-                if (enemyAgent.remainingDistance <= enemyAgent.stoppingDistance && !enemyAgent.pathPending)
+                if (enemyAgent.remainingDistance <= enemyAgent.stoppingDistance /*&& !enemyAgent.pathPending*/)
                 {
                     state = STATE.ATTACK;
+                }
+                else if (ZombieCanSeePlayer())
+                {
+                    state = STATE.WANDER;
+                    enemyAgent.ResetPath();
                 }
                 break;
 
@@ -153,3 +183,145 @@ public class ZombieController : MonoBehaviour
     //    }
     //}
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.AI;
+
+///*
+// FINITE STATE MACHINE
+//-it is an artificial inteligence system to store state's data and transition between them, example: animation system.
+//-A finite state machine that defines all the states they can be in and how they can get from one state to another state.
+//-determine the states of the character.
+//-Zombie we determined five states.
+//1. idle, 2. wander , 3. chase, 4. attack, 5. death. 
+
+
+// */
+
+//public class ZombieController : MonoBehaviour
+//{
+//    Animator anim;
+//    public GameObject targetPlayer;
+//    private NavMeshAgent agent;
+//    public float walkingSpeed;
+//    public float runningSpeed;
+
+
+//    enum STATE { IDLE, WANDER, CHASE, ATTACK, DEAD };
+
+//    STATE state = STATE.IDLE;
+
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        anim = this.GetComponent<Animator>();
+//        agent = this.GetComponent<NavMeshAgent>();
+//        ///anim.SetBool("isWalking", true);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//        switch (state)
+//        {
+//            case STATE.IDLE:
+//                if (isVisible())
+//                {
+//                    state = STATE.CHASE;
+//                }
+//                else
+//                {
+//                    state = STATE.WANDER;
+//                }
+
+
+//                break;
+//            case STATE.WANDER:
+//                if (!agent.hasPath)
+//                {
+//                    float newRandompositionX = this.transform.position.x + Random.Range(-10, 10);
+//                    float newRandompositionZ = this.transform.position.z + Random.Range(-10, 10);
+//                    float newRandompositionY = Terrain.activeTerrain.SampleHeight(new Vector3(newRandompositionX, 0, newRandompositionZ));
+//                    Vector3 finalDestination = new Vector3(newRandompositionX, newRandompositionY, newRandompositionZ);
+//                    agent.SetDestination(finalDestination);
+//                    agent.stoppingDistance = 0f;
+//                    TrunOffAnimtriggers();
+//                    agent.speed = walkingSpeed;
+//                    anim.SetBool("isWalking", true);
+//                }
+//                if (isVisible())
+//                {
+//                    state = STATE.CHASE;
+//                }
+
+//                break;
+//            case STATE.CHASE:
+//                agent.SetDestination(targetPlayer.transform.position);
+//                agent.stoppingDistance = 2.0f;
+//                TrunOffAnimtriggers();
+//                agent.speed = runningSpeed;
+//                anim.SetBool("isRunning", true);
+//                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+//                {
+//                    state = STATE.ATTACK;
+//                }
+//                else if (ZombieCanSeeplayer())
+//                {
+//                    state = STATE.WANDER;
+//                    agent.ResetPath();
+//                }
+//                break;
+//            case STATE.ATTACK:
+//                TrunOffAnimtriggers();
+//                anim.SetBool("isAttacking", true);
+//                transform.LookAt(targetPlayer.transform);
+//                if (DistanceToThePlayer() > agent.stoppingDistance + 2f)
+//                {
+//                    state = STATE.CHASE;
+//                }
+//                break;
+//            case STATE.DEAD:
+//                break;
+//            default:
+//                break;
+//        }
+//    }
+//    bool ZombieCanSeeplayer()
+//    {
+//        if (DistanceToThePlayer() > 10)
+//        {
+//            return true;
+//        }
+//        return false;
+//    }
+
+//    void TrunOffAnimtriggers()
+//    {
+//        anim.SetBool("isWalking", false);
+//        anim.SetBool("isRunning", false);
+//        anim.SetBool("isAttacking", false);
+//        anim.SetBool("isDead", false);
+//    }
+
+//    private float DistanceToThePlayer()
+//    {
+//        return Vector3.Distance(targetPlayer.transform.position, this.
+//            transform.position);
+//    }
+//    bool isVisible()
+//    {
+//        //logic for zombie to see the player and chase
+//        if (DistanceToThePlayer() < 10f)
+//        {
+//            return true;
+//        }
+//        else
+//        {
+//            return false;
+//        }
+//    }
+//}
